b0c37e5a7d21adc51f0fc2060e394dfd5779e7d0
knoph1 <knopholuoch@yahoo.com>
2025-01-31 01:59:18 +0300

    Javascript rectification

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,15 +64,3 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
             }
         );
 });
-
-<
-script >
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-            targetElement.scrollIntoView({ behavior: 'smooth' });
-        });
-    }); <
-/script>

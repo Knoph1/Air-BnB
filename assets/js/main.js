8b88169b26a070c152c454417626b8850568c327
knoph1 <knopholuoch@yahoo.com>
2025-01-31 01:55:32 +0300

    First commit after Pre-release

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,3 +64,15 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
             }
         );
 });
+
+<
+script >
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
+    }); <
+/script>
